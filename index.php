015446cc49cc29a638f5300daa61275380d9fe33
JAshwini <joshiashwini39@gmail.com>
2017-08-17 15:25:27 +0530

    v6

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>twitter signup</title>
+		<title>Tweets World</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<center>
 			<div class="container" style="padding-top:5%;">
 				<div class="row">
-					<div class="col-sm-12 col-md-12 col-lg-12">
-						<!-- <span ><img src="assets/images/t.png" alt="" style="border-radius: 50%;" /></span> -->
-					</div>
 					<div class="col-sm-12 col-m	d-12 col-lg-12" style="padding-top:5%;">
-						<!-- <h2>RTcamp Assignment | Twitter Login</h2> -->
-						<!-- <p>See your 'Tweets', 'Timeline Tweets' <br/>and 'Followers' after authentication</p> -->
+						<h2> Welcome to Tweets World!</h2>
 					</div>
 					<div class="col-sm-12 col-md-12 col-lg-12" style="">
 						<a href="http://127.0.0.1/rtwitter-master/ui/home.php" >
